68e2891e82feb523aca2a9360841fc621af00fba
dgerzetich <95722520+dgerzetich@users.noreply.github.com>
2021-12-09 23:29:22 -0600

    12/09/2021

--- comments/commentsOutline.py
+++ comments/commentsOutline.py
@@ -5,13 +5,19 @@ Fill in this outline as the instructor goes through the lesson.
 
 #1) Make a single line comment.
 
-
+#This is a single line comment
 
 #2) Make a multi-line comment.
 
-
+'''
+This 
+is 
+a 
+multi-line
+comment
+'''
 
 #3) Make an int variable. Then, make a single line comment on the 
 #same line, after the variable.
 
-
+var = 2 #This is an integer variable

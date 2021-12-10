6b98c062855f8b6fb5cd37edc6680c41da630df1
dgerzetich <95722520+dgerzetich@users.noreply.github.com>
2021-12-09 23:49:37 -0600

    12/09/2021

---
+++ survey/survey.py
@@ -0,0 +1,29 @@
+'''
+Created on Dec 9, 2021
+
+@author: Daria Gerzetich
+'''
+'''
+The Objective is to make a program that can take five different questions and 
+get their answers from a user, store the user’s answers in variables, and print
+the answers.
+'''
+name = input("What is your name?") 
+print(name)
+
+
+color = input("What is your favorite color?") 
+print(color)
+
+
+city = input("What city did you grow up in?")  
+print(city)
+
+
+friend = input("What is your best friends name?") 
+print(friend)
+
+
+hair = input("What color hair do you have?")  
+print(hair)
+

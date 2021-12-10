6158bad9291f5dc808e4a1348613ab1b50c7e361
dgerzetich <95722520+dgerzetich@users.noreply.github.com>
2021-12-09 20:47:33 -0600

    12/09/2021

--- finallyDone/finallyDone.py
+++ finallyDone/finallyDone.py
@@ -3,3 +3,17 @@ Created on Dec 7, 2021
 
 @author: ITAUser
 '''
+
+'''
+userResponse = input(text_to_user)
+'''
+
+favColor = input("What is your favorite color?")
+
+age = input("What is your age")
+age = int(age)
+
+age=int(input("What is your age?"))
+
+''' There is syntax errors and logical errors'''
+

2a623bb0f1fcd05b34518bb4fa7f82c25400c322
dgerzetich <95722520+dgerzetich@users.noreply.github.com>
2021-12-09 23:33:20 -0600

    12/09/2021

--- comments/debugCommentDrills.py
+++ comments/debugCommentDrills.py
@@ -15,12 +15,12 @@ an object, when the user inputs height, width, and depth.
 
 The function should print the sentence plus the volume and
 return the volume.
-
+'''
 def volumeCalculator(height, width, depth):
     area = height * width
     volume = depth * area
     sentence = "The volume of this object is: "
-    This prints out the sentence with the calculated volume.
+    #This prints out the sentence with the calculated volume.
     print(sentence + volume)
     return volume
 

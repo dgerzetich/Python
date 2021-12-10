a9451ae10714991ba7317f9559927ea99bb7f51e
dgerzetich <95722520+dgerzetich@users.noreply.github.com>
2021-12-09 22:30:04 -0600

    12/09/2021

---
+++ variables/variablesOutline.py
@@ -0,0 +1,78 @@
+'''
+This outline will help solidify concepts from the Variables lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+from pickle import TRUE
+
+#1)
+#Make an example variable of each of the types of variables (int, float, 
+#string, boolean and list):
+
+#Integer
+a = 5
+print(a)
+
+#Float
+a = 3.45
+print(a)
+
+#String 
+a = "Samantha"
+print(a)
+
+#Boolean
+a = True
+print(a)
+
+#List
+a = [1,2,3,4]
+print(a)
+
+#2)
+#Make two variables one named 'a' and one named 'A' both set to  different data:
+#(Remember that capitalization matters, so these can be two different variables)
+
+varA = 1
+vara = 2
+print(varA)
+print(vara)
+
+#3)
+#Make a variable, and then go down a couple lines and change the data in that
+#same variable:
+
+var = 4
+var = "string"
+print(var)
+
+#4)
+#Make a variable and set it to a variable you have already created:
+
+varTwo = varA
+print(varTwo)
+
+#5)
+#Make a list with three items. Make a variable that is set to the second
+#item in that list:
+
+varz=42
+l = [varA, varz, vara]
+print(l[1])
+
+#6)
+#Make a String set to your name. Make a variable that is set to the first
+#character in that string:
+
+string = "Daria"
+D = "pickles"
+print(string[0])
+
+#7)
+#Print out NE of any of the variables you have created so far:
+
+print(D)
+
+#8)
+#Make a variable of any type, with one of the common SYNTAX errors
+
+var == 4

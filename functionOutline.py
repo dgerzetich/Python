8eaa9db1bde4f80c13991d546ff7ab2705a0ac42
dgerzetich <95722520+dgerzetich@users.noreply.github.com>
2022-04-06 22:25:20 -0500

    4/6/22

--- functionOutline.py
+++ functionOutline.py
@@ -2,27 +2,33 @@
 This outline will help solidify concepts from the Functions lesson.
 Fill in this outline as the instructor goes through the lesson.
 '''
-from builtins import True
+
 
 #1) Make a function that has two boolean parameters. If both booleans are
 #true, return true. Else, return false. Then, call the function.
 #Print what the function returns.
 
-def boolPara(s, p):
-    s == True
-    p == False
+def boolPara():
+    s = bool(1)
+    p = bool(2)
     if s == True and p == True:
         print("True")
     else:
         print("False")
-     return   
- boolPara()
+    return   
+boolPara()
 
 #2) Make a function that takes one int parameter: gallons. Convert gallons
 #to cups (do this by multiplying gallons by 16). Then return cups. Then,
 #call the function.
 #Print what the function returns.
-def convertGallons(gallons):
+def convertGallons():
+    gallons = 4
+    cups = gallons * 16
+    print(cups)
+    return cups
+    
+convertGallons()
 
 
 #3) Make a function of any any kind with a common SYNTAX error. Then, call the
